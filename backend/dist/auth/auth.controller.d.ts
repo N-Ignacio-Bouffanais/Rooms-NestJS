1c59f6ae1df3fb248f559a87e77d4d27653e76b4
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-09-13 23:42:28 -0300

    [hotfix] delete folder dist

--- backend/dist/auth/auth.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService } from './auth.service';
-import { LoginDto, RegisterDto } from './dto';
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    signIn(loginDto: LoginDto): Promise<any>;
-    signUp(registerDto: RegisterDto): Promise<any>;
-}
--- backend/dist/prisma/prisma.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-import { ConfigService } from '@nestjs/config';
-export declare class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-    constructor(config: ConfigService);
-    onModuleInit(): Promise<void>;
-    onModuleDestroy(): Promise<void>;
-}
