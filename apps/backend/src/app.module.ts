182929edd95c4847d8fc521b8d8991a5bf4dfecb
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-09-14 22:56:00 -0300

    [monorepo] Turbo implement , run dev, build and start works, but home page doesnt work well, astro works on the other pages

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,13 +4,20 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
-  providers: [
-    AppService,
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'frontend/dist'),
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    PrismaModule,
   ],
+  providers: [AppService],
   controllers: [AppController],
 })
 export class AppModule {}
