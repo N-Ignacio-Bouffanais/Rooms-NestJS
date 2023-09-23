279b009d42f975e25874dfa1b8724defb889f78c
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-09-23 17:25:40 -0300

    [feature] Login and Register styles

--- apps/frontend/src/components/AuthForm.tsx
+++ apps/frontend/src/components/AuthForm.tsx
@@ -1,24 +1,25 @@
-
-
 function AuthForm() {
   return (
-    <div className="">
-      <form className="flex flex-col">
-        <label htmlFor="">Correo</label>
-        <input type="email" />
-        <label htmlFor="">Contraseña</label>
-        <input type="password" />
-        <label htmlFor="">Eres Estudiante o Profesor?</label>
-        <select name="rol" id="rol">
-          <option value="estudiante">Estudiante</option>
-          <option value="profesor">Profesor</option>
-        </select>
-        <label>Acepto los términos y condiciones</label>
-        <input type="checkbox" required />
-        <button type="submit" className="">Enviar</button>
-      </form>
-    </div>
+    <form className="flex flex-col">
+      <label htmlFor="">Correo</label>
+      <input type="email" />
+      <label htmlFor="">Contraseña</label>
+      <input type="password" />
+      <label htmlFor="">Eres Estudiante o Profesor?</label>
+      <select name="rol" id="rol">
+        <option value="estudiante">Estudiante</option>
+        <option value="profesor">Profesor</option>
+      </select>
+      <label>Acepto los términos y condiciones</label>
+      <input type="checkbox" required />
+      <button
+        type="submit"
+        className="flex justify-center items-center w-36 bg-sky-600 h-10 p-3 m-3 rounded-xl text-white font-medium hover:bg-sky-700"
+      >
+        Enviar
+      </button>
+    </form>
   );
 }
 
-export default AuthForm
+export default AuthForm;
--- apps/frontend/src/routes/Register.tsx
+++ apps/frontend/src/routes/Register.tsx
@@ -3,24 +3,17 @@ import AuthForm from "../components/AuthForm";
 
 const Register = () => {
   return (
-    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
-      <div className="flex flex-col items-center space-y-2 text-center">
+    <div className="flex w-full flex-col justify-center bg-slate-400 h-[91dvh]">
+      <div className="flex flex-col items-center mx-auto space-y-2 text-center py-6 bg-white rounded-xl w-[340px]">
         <h1 className="text-2xl font-semibold tracking-tight">Bienvenido</h1>
         <AuthForm />
-        <p className="text-sm max-w-xs mx-auto">
-          Al continuar esta aceptando nuestro Acuerdo de usuario y Política de
-          privacidad.
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          Ya tienes una cuenta?{" "}
+          <Link to="/login" className="hover:text-brand text-sm text-sky-700">
+            Ingresar
+          </Link>
         </p>
       </div>
-      <p className="px-8 text-center text-sm text-muted-foreground">
-        Ya tienes una cuenta?{" "}
-        <Link
-          to="/login"
-          className="hover:text-brand text-sm underline underline-offset-4"
-        >
-          Ingresar
-        </Link>
-      </p>
     </div>
   );
 };
