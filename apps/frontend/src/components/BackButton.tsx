4ed54cd5272af30c77b36e43f3f03e87ec4956fd
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-09-30 20:14:53 -0300

    [feature] icon changes exit and back

--- apps/frontend/src/components/BackButton.tsx
+++ apps/frontend/src/components/BackButton.tsx
@@ -1,6 +1,5 @@
-import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
-
+import {AiOutlineArrowLeft} from "react-icons/ai";
 
 function BackButton() {
   return (
@@ -9,7 +8,7 @@ function BackButton() {
       className="flex justify-center items-center w-80 bg-[#100f1d] h-10 p-3 rounded-full text-white text-lg font-medium hover:bg-black"
     >
       <i className="mr-1">
-        <BsFillArrowLeftCircleFill />
+        <AiOutlineArrowLeft />
       </i>
       Regresar
     </Link>
--- apps/frontend/src/routes/student/Dashboard.tsx
+++ apps/frontend/src/routes/student/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { useAuthStore } from "../../store/auth";
 import {BsFillCalendarDateFill} from "react-icons/bs"
 import { GrNotes, GrScorecard } from "react-icons/gr";
+import {BiExit} from "react-icons/bi"
 import {MdTask} from "react-icons/md";
 import { Link } from "react-router-dom";
 
@@ -9,42 +10,44 @@ const Dashboard = () => {
   const lastname = useAuthStore((state)=>state.lastname);
 
   return (
-    <div className="flex flex-col p-8 h-[100dvh] sm:p-12">
-      <h1 className="flex justify-start font-semibold text-xl">
-        Estudiante: {`${firstname} ${lastname}`}
-      </h1>
-      <div className="grid grid-cols-2 grid-rows-2 gap-3 my-6">
-        <Link to={""} title="Mi horario">
-          <div className="flex justify-center items-center w-40 h-40 bg-[#FF0060] rounded-xl">
-            <i>
-              <BsFillCalendarDateFill className="w-12 h-12" />
-            </i>
-          </div>
-        </Link>
-        <Link to={""} title="Mis notas">
-          <div className="flex justify-center items-center w-40 h-40 bg-[#0079FF] rounded-xl">
-            <i>
-              <GrNotes className="w-12 h-12" />
-            </i>
-          </div>
-        </Link>
-        <Link to={""}>
-          <div className="flex justify-center items-center w-40 h-40 bg-[#00DFA2] rounded-xl">
-            <i>
-              <GrScorecard className="w-12 h-12" />
-            </i>
-          </div>
-        </Link>
-        <Link to={""}>
-          <div className="flex justify-center items-center w-40 h-40 bg-[#40128B] rounded-xl">
-            <i>
-              <MdTask className="w-12 h-12" />
-            </i>
-          </div>
-        </Link>
-      </div>
-      <div>
-        <button className="font-semibold">Salir</button>
+    <div className="flex flex-col h-[100dvh] ">
+      <div className="w-[90dvw] mx-auto">
+        <h1 className="flex justify-start font-semibold text-xl my-4">
+          Estudiante: {`${firstname} ${lastname}`}
+        </h1>
+        <div className="grid grid-cols-2 grid-rows-2 gap-y-3 my-6">
+          <Link to={""} title="Mi horario">
+            <div className="flex justify-center items-center w-40 h-40 bg-[#FF0060] rounded-xl">
+              <i>
+                <BsFillCalendarDateFill className="w-12 h-12" />
+              </i>
+            </div>
+          </Link>
+          <Link to={""} title="Mis notas">
+            <div className="flex justify-center items-center w-40 h-40 bg-[#0079FF] rounded-xl">
+              <i>
+                <GrNotes className="w-12 h-12" />
+              </i>
+            </div>
+          </Link>
+          <Link to={""}>
+            <div className="flex justify-center items-center w-40 h-40 bg-[#00DFA2] rounded-xl">
+              <i>
+                <GrScorecard className="w-12 h-12" />
+              </i>
+            </div>
+          </Link>
+          <Link to={""}>
+            <div className="flex justify-center items-center w-40 h-40 bg-[#40128B] rounded-xl">
+              <i>
+                <MdTask className="w-12 h-12" />
+              </i>
+            </div>
+          </Link>
+        </div>
+        <div>
+          <button className="font-semibold">Salir<BiExit/></button>
+        </div>
       </div>
     </div>
   );
