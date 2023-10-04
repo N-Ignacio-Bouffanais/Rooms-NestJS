e4626d93d970cc1f20e406c263ec3fa1a06a7fd8
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-10-04 15:24:31 -0300

    [config] prisma schema, subject limit not necesary and dto on subject either

--- apps/backend/src/subjects/subjects.controller.ts
+++ apps/backend/src/subjects/subjects.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SubjectsService } from './subjects.service';
-import { CreateSubjectDto } from './dto/create-subject.dto';
-import { UpdateSubjectDto } from './dto/update-subject.dto';
 
 @Controller('subjects')
 export class SubjectsController {
