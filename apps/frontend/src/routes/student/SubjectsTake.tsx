d215d5b6cfc819032e04edfa1ceb902d477bfab2
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-10-04 21:28:48 -0300

    [feature] render div with subjects

--- apps/frontend/src/routes/student/SubjectsTake.tsx
+++ apps/frontend/src/routes/student/SubjectsTake.tsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react"
 import { useAppStore } from "../../store/app"
 
+interface Subject{
+  name: string
+  id: string
+}
 
 function SubjectsTake() {
 
@@ -16,8 +20,15 @@ function SubjectsTake() {
   return (
     <div className="h-[100dvh]">
       <h1 className="text-3xl font-bold">Toma de ramos</h1>
+      <div>
+        {subjects.map((subject: Subject) => (
+          <ul className="" key={subject.id}>
+            <li>{subject.name}</li>
+          </ul>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default SubjectsTake
