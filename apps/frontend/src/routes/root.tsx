4ef8730e0a62305f15a0df6adcc31c1b39f1089e
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-10-20 18:32:58 -0300

    [css root] changes to css on the root route, gradient css and better responsive

--- apps/frontend/src/routes/root.tsx
+++ apps/frontend/src/routes/root.tsx
@@ -4,29 +4,29 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex items-center justify-center bg-[#526D82] h-[100dvh] w-full">
-      <div className=" bg-[#F0F0F0] h-[90dvh] w-[90dvw] mx-auto rounded-xl p-8 sm:p-12 ">
-        <h1 className="justify-center text-4xl flex flex-wrap font-bold my-6 sm:text-5xl">
+    <div className="flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 h-[100dvh] w-full">
+      <div className=" bg-[#F0F0F0] w-[85dvw] mx-auto rounded-xl p-8 sm:p-12 mb-6 md:w-[80dvw] lg:w-[75dvw]">
+        <h1 className="justify-center text-4xl flex flex-wrap font-bold mt-3 mb-4 sm:text-5xl md:text-6xl md:mb-6">
           Bienvenido a{" "}
           <span className="flex items-center text-[#2192FF]">
             <DiAtom className="h-8 w-8" />
             AtomCenter
           </span>
         </h1>
-        <p className="font-medium">
+        <p className="font-medium sm:text-lg text-justify my-2">
           En esta pagina institucional podra subir trabajos en formato PDF, PNG,
           JPEG y mas utilizando el rol <span>Estudiante</span> y podra
           calificarlos como <span>Profesor.</span> Ademas cada trabajo estara
           asignado a una clase en particular.
         </p>
 
-        <div className="flex flex-wrap items-center justify-center mt-6">
+        <div className="flex flex-col items-center mt-6">
           <button
             type="button"
             onClick={() => {
               navigate("/estudiante/login");
             }}
-            className="flex justify-center items-center w-44 bg-[#4942E4] h-10 p-3 m-3 rounded-xl text-white font-medium hover:bg-[#40128B]"
+            className="flex justify-center items-center w-52 bg-[#4942E4] h-10 p-3 m-3 rounded-xl text-white font-medium hover:bg-[#40128B]"
           >
             Soy Estudiante
           </button>
@@ -35,7 +35,7 @@ const Home = () => {
             onClick={() => {
               navigate("/profesor/login");
             }}
-            className="flex justify-center items-center w-44 bg-[#05BFDB] h-10 p-3 m-3 rounded-xl text-white font-medium hover:bg-[#016A70]"
+            className="flex justify-center items-center w-52 bg-[#05BFDB] h-10 p-3 m-3 rounded-xl text-white font-medium hover:bg-[#016A70]"
           >
             Soy Profesor
           </button>
