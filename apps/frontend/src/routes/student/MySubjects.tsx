f03461c6dc315104c80160ab27885e1d13896860
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-10-20 16:16:43 -0300

    [css mysubjects] css table ready

--- apps/frontend/src/routes/student/MySubjects.tsx
+++ apps/frontend/src/routes/student/MySubjects.tsx
@@ -21,19 +21,23 @@ function MySubjects() {
       <h1 className="flex justify-start font-bold text-3xl text-blue-900 my-6">
         Mis Ramos
       </h1>
-      <table className="w-full mb-10 mt-4">
-        <tbody>
-          <tr className="text-xl">
-            <th className="text-left">Clase</th>
-            <th className="xs:w-80">Accion</th>
+      <table className="w-full mb-10 mt-4 bg-white rounded-2xl p-6">
+        <thead>
+          <tr className="text-xl grid grid-cols-2 h-12 items-center border-y">
+            <th>Clase</th>
+            <th>Accion</th>
           </tr>
+        </thead>
+        <tbody>
           {InsSubjects.map((subject: Subject) => (
             <tr
-              className="font-medium h-12 "
+              className=" font-semibold text-lg grid grid-cols-2 auto-rows-auto"
               key={subject.id}
             >
-              <td className="font-semibold">{subject.name}</td>
-              <td className="flex justify-center xs:w-80">
+              <td className="flex justify-center items-center">
+                {subject.name}
+              </td>
+              <td className="flex justify-center items-center">
                 <button
                   className="w-16 h-11 my-3 rounded-lg text-white bg-[#0177fb] mx-2"
                   onClick={(e) => {
