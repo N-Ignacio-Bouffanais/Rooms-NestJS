cc4d9b31f0dce9d1dd0321dfc34e0ad34f258d0a
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-10-22 18:38:54 -0300

    [feature] modal, but doesnt work well

--- apps/frontend/src/routes/student/SubjectsTake.tsx
+++ apps/frontend/src/routes/student/SubjectsTake.tsx
@@ -1,26 +1,26 @@
-import { useEffect } from "react"
-import { useAppStore } from "../../store/app"
-import axios from "axios"
+import { useEffect } from "react";
+import { useAppStore } from "../../store/app";
+import axios from "axios";
+import { RxCross2 } from "react-icons/rx";
 
- export type Subject ={
-  name: string
-  id: string
-  limit: number
-}
+export type Subject = {
+  name: string;
+  id: string;
+  limit: number;
+};
 
 function SubjectsTake() {
-
-  const {getAllSubjects, subjects} = useAppStore()
+  const { getAllSubjects, subjects } = useAppStore();
   const firstname = useAppStore((state) => state.firstname);
   const lastname = useAppStore((state) => state.lastname);
   //const email = useAppStore((state) => state.email);
-  const setModal = useAppStore((state) => state.setModal)
+  const { inscrpModal, setModal } = useAppStore();
 
   useEffect(() => {
-    return ()=> {
+    return () => {
       getAllSubjects(firstname, lastname);
-      console.log(subjects)
-    }
+      console.log(subjects);
+    };
   }, []);
 
   // const Inscription = async () => {
@@ -31,20 +31,24 @@ function SubjectsTake() {
   //   // console.log(res);
   //   console.log(e)
   // };
-  const Modal = async(e: React.SyntheticEvent<EventTarget>) => {
+  const Modal = async (e: React.SyntheticEvent<EventTarget>) => {
     const selected = (e.target as HTMLButtonElement).id;
-    console.log(selected)
-    console.log(setModal);
+    setModal(inscrpModal);
   };
 
   return (
     <>
-    {}
-    <div id="modal" className="">
-      <div>
-        <p>dentro del modal</p>
+      <div className="fixed inset-0 bg-zinc-900/20 z-10">
+        <div className="container flex items-center h-full max-w-lg mx-auto">
+          <div className="relative bg-white w-full h-fit py-20 px-2 rounded-lg">
+            <div className="absolute top-4 right-4">
+              <button onClick={() => setModal(false)}>
+                <RxCross2 />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
       <div className="h-[100dvh] w-[100dvw]">
         <div className="mx-auto w-[90dvw] sm:w-[85dvw] md:w-[80dvw]">
           <h1 className="text-4xl font-bold my-8">Toma de ramos</h1>
@@ -77,4 +81,4 @@ function SubjectsTake() {
   );
 }
 
-export default SubjectsTake
+export default SubjectsTake;
