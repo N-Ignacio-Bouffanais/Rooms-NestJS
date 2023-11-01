7b911ca3fad3891ac4f5243ec99fe65be1247010
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-10-31 21:37:55 -0300

    [feature] dark mode config

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useAppStore } from "../store/app";
 import {DiAtom} from "react-icons/di";
+import { BsMoonStarsFill } from "react-icons/bs";
+import { useEffect, useState } from "react";
 
 interface Props {
   redirectTo: string;
@@ -9,17 +11,37 @@ interface Props {
 function Navbar(props:Props) {
   const navigate = useNavigate();
   const logout = useAppStore((state) => state.logout);
+  const [theme,setTheme] = useState("light")
+
+  useEffect(()=>{
+    if(theme === 'dark'){
+      document.querySelector('html')?.classList.add('dark')
+    } else{
+      document.querySelector('html')?.classList.remove('dark')
+    }
+  },[theme])
+
+  const HandleTheme = () =>{
+    setTheme(prevTheme => prevTheme === 'light'? 'dark' : 'light')
+  }
 
   return (
-    <nav className="flex bg-gray-700 h-[9dvh] w-full text-white font-medium">
+    <nav className="flex bg-gray-700 h-[9dvh] w-full text-white font-medium dark:bg-red-700">
       <ul className="flex w-[90dvw] mx-auto justify-between items-center">
         <li>
-          <Link className="flex items-center" to={props.redirectTo} title="Panel de control">
+          <Link
+            className="flex items-center"
+            to={props.redirectTo}
+            title="Panel de control"
+          >
             <DiAtom className="h-8 w-8" />
             AtomCenter
           </Link>
         </li>
-        <li>
+        <li className="flex items-center">
+          <button className="mr-5" onClick={HandleTheme}>
+            <BsMoonStarsFill />
+          </button>
           <button
             onClick={() => {
               logout(), navigate("/");
--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -2,6 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
