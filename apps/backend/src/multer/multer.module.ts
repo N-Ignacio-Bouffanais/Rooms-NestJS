c4e0577e303bd7f95babdae5646679a96bdf0684
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-11-17 13:54:56 -0300

    upload file filename fix

--- apps/backend/src/multer/multer.module.ts
+++ apps/backend/src/multer/multer.module.ts
@@ -9,14 +9,12 @@ import { diskStorage } from 'multer';
       storage: diskStorage({
         destination: './uploads', // specify the destination folder
         filename: (req, file, callback) => {
-          const uniqueSuffix =
-            Date.now() + '-' + Math.round(Math.random() * 1e9);
           const originalname = file.originalname;
           const extension = originalname.substring(
             originalname.lastIndexOf('.'),
             originalname.length,
           );
-          callback(null, uniqueSuffix + extension);
+          callback(null, originalname + extension);
         },
       }),
     }),
